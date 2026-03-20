6da90deb65d71892ef1e270665b1b3b0a9e2084b
toninojic <antonijenojic01@gmail.com>
2026-03-20 19:01:18 +0100

    adjusting ecosysytem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,36 @@
 module.exports = {
     apps: [
         {
-            name: 'analytics-production',
-            script: 'server.js',
+            name: "analytics-production",
+            script: "server.js",
+            cwd: "/var/www/analytics-app",
+            instances: 1,
+            exec_mode: "fork",
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "300M",
             env: {
-                NODE_ENV: 'production',
-                RUN_MODE: 'single',
-            },
+                NODE_ENV: "production",
+                RUN_MODE: "single",
+                PORT: 8647,
+                STAGING_PORT: 8648
+            }
         },
         {
-            name: 'analytics-staging',
-            script: 'server.js',
+            name: "analytics-staging",
+            script: "server.js",
+            cwd: "/var/www/analytics-app",
+            instances: 1,
+            exec_mode: "fork",
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "300M",
             env: {
-                NODE_ENV: 'staging',
-                RUN_MODE: 'single',
-            },
-        },
-    ],
+                NODE_ENV: "staging",
+                RUN_MODE: "single",
+                PORT: 8647,
+                STAGING_PORT: 8648
+            }
+        }
+    ]
 };
