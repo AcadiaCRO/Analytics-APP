e5d6bc382a150987a44fa3d43539340016ba6d65
toninojic <antonijenojic01@gmail.com>
2026-03-26 16:06:12 +0100

    adding autodeploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,36 +1,36 @@
 module.exports = {
     apps: [
         {
-            name: "analytics-production",
-            script: "server.js",
-            cwd: "/var/www/analytics-app",
+            name: 'analytics-production',
+            script: 'server.js',
+            cwd: '/var/www/analytics-app',
             instances: 1,
-            exec_mode: "fork",
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
-            max_memory_restart: "300M",
+            max_memory_restart: '300M',
             env: {
-                NODE_ENV: "production",
-                RUN_MODE: "single",
+                NODE_ENV: 'production',
+                RUN_MODE: 'single',
                 PORT: 8647,
-                STAGING_PORT: 8648
-            }
+                STAGING_PORT: 8648,
+            },
         },
         {
-            name: "analytics-staging",
-            script: "server.js",
-            cwd: "/var/www/analytics-app",
+            name: 'analytics-staging',
+            script: 'server.js',
+            cwd: '/var/www/analytics-app',
             instances: 1,
-            exec_mode: "fork",
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
-            max_memory_restart: "300M",
+            max_memory_restart: '300M',
             env: {
-                NODE_ENV: "staging",
-                RUN_MODE: "single",
+                NODE_ENV: 'staging',
+                RUN_MODE: 'single',
                 PORT: 8647,
-                STAGING_PORT: 8648
-            }
-        }
-    ]
+                STAGING_PORT: 8648,
+            },
+        },
+    ],
 };
